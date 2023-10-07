de7d2701a92b13f9d3734ea17d3e55181a67bc71
Sargam <sargampoudel100@gmail.com>
2023-10-08 04:23:55 +0530

    fix: setup prisma for nest

---
+++ src/prisma/prisma.module.ts
@@ -0,0 +1,9 @@
+import { Global, Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+
+@Global()
+@Module({
+  exports: [PrismaService],
+  providers: [PrismaService],
+})
+export class PrismaModule {}
