7301e0e96e69ed40ae0365766df6f4a0eb0428cd
Sargam <sargampoudel100@gmail.com>
2023-10-11 14:44:13 +0530

    deps: rm `@nestjs-module/mailer` due to vm2 issues

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,20 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { ConfigService } from '@nestjs/config';
-import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
-  imports: [
-    MailerModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: async (config: ConfigService) => ({
-        transport: `smtp://
-        ${config.get('GOOGLE_AUTH_USERNAME')}:
-        ${config.get('GOOGLE_AUTH_PASSWORD')}
-        @smtp.gmail.com`,
-      }),
-    }),
-  ],
   providers: [MailService, ConfigService],
   exports: [MailService],
 })
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,23 +1,27 @@
-import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { IUserFromDb } from 'src/users/users.service';
+import { Transporter, createTransport } from 'nodemailer';
 
 @Injectable()
 export class MailService {
-  constructor(
-    private readonly mailerService: MailerService,
-    private readonly configService: ConfigService,
-  ) {}
+  private transporter: Transporter;
+
+  constructor(private readonly configService: ConfigService) {
+    this.transporter = createTransport(`smtp://
+        ${configService.get('GOOGLE_AUTH_USERNAME')}:
+        ${configService.get('GOOGLE_AUTH_PASSWORD')}
+        @smtp.gmail.com`);
+  }
 
   async sendUserConfirmation(user: IUserFromDb, token: string) {
     const serverUrl = this.configService.get<string>('SERVER_URL');
     const url = `${serverUrl}/auth/confirm?token=${token}`;
 
-    await this.mailerService.sendMail({
+    await this.transporter.sendMail({
       to: user.email,
       subject: 'Welcome to Test App',
-      text: `<a href='${url}'>Click Here!</a>`,
+      html: `<a href='${url}'>Click Here!</a>`,
     });
   }
 }
