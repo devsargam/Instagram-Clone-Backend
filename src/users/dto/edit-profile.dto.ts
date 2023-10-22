c6d698a37531ee8355fda8d981efdb8f018a0a7f
Sargam <sargampoudel100@gmail.com>
2023-10-22 12:48:03 +0545

    fix: update prefernces to be not optional

--- src/users/dto/edit-profile.dto.ts
+++ src/users/dto/edit-profile.dto.ts
@@ -4,17 +4,17 @@ export const editProfileSchema = z.object({
   website: z
     .string()
     .url()
-    .max(128, 'Link must be no longer than 128 characters')
-    .optional(),
-  bio: z
-    .string()
-    .max(150, 'Bio must be no longer than 150 characters')
-    .optional(),
+    .max(128, 'Link must be no longer than 128 characters'),
+  bio: z.string().max(150, 'Bio must be no longer than 150 characters'),
   gender: z
-    .enum(['male', 'female', 'rather not say'])
-    .default('rather not say'),
+    .enum(['MALE', 'FEMALE', 'RATHER_NOT_SAY'])
+    .default('RATHER_NOT_SAY')
+    .transform((str) => str.toUpperCase()),
   receiveMarkettingEmails: z.boolean().default(true),
-  accountType: z.enum(['private', 'public']).default('public'),
+  accountType: z
+    .enum(['PRIVATE', 'PUBLIC'])
+    .default('PUBLIC')
+    .transform((str) => str.toUpperCase()),
 });
 
 export type editProfileDto = z.infer<typeof editProfileSchema>;
